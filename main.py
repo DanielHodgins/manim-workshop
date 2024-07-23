36c5c539753261fcd468df2d3157c2fd8c8b9134
DanielHodgins <121420574+DanielHodgins@users.noreply.github.com>
2024-07-23 06:04:12 +0000

    yes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class HelloWorld(Scene):
     def construct(self):
         hello = MathTex(r"\text{Hello World!}")
         circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        circle.set_fill(DARK_BLUE, opacity=0.5)  # set the color and transparency
         square = Square(side_length=2.0, color=BLUE, fill_color=TEAL, fill_opacity=0.5)
         welcome = MathTex(r"\text{Welcome to MAA SE 2024!}")
 
@@ -16,6 +16,9 @@ class HelloWorld(Scene):
         self.play(Transform(hello, square))
         self.wait(3)
         self.play(Transform(hello, welcome))
+        self.wait(5)
+        name = MathTex(r"\text{My name is YOUR NAME HERE.}")
+        self.play(Transform(hello, name))
         self.wait(5)
 
 
